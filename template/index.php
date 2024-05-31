378dc78df51d46fcd900470311b0ad77e15a6098
macielemilly <emillymaciel0123@gmail.com>
2024-05-31 14:03:00 -0300

    pag inicio1

--- template/index.php
+++ template/index.php
@@ -1,7 +1,76 @@
 <?php
 session_start();
- if(!isset($_SESSION['id'])){
+if (!isset($_SESSION['id'])) {
     header('Location: ../cadastro_login/login.php');
 }
 require 'nav.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .inicio-title {
+            margin-top: 60px;
+            /* Adiciona margem superior igual à altura da barra de navegação */
+        }
+
+        .text {
+            font-size: 25px;
+        }
+    </style>
+</head>
+
+<body style="height: 100vh;">
+    <div class="mx-5">>
+        <h1 class="inicio-title text-danger">Início</h1>
+    </div>
+    <div class="d-flex flex-row justify-content-center mt-5">
+        <div class="d-flex justify-content-center mx-4">
+            <div class="container d-flex justify-content-center">
+                <div class="card card text-dark bg-danger mb-3" style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text">Produtos</h5>
+                        <p class="card-text">Futuro texto.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="text-decoration-none text-dark">Ver mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <div class="container d-flex justify-content-center mx-4">
+                <div class="card card text-dark bg-danger mb-3" style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text">Categorias</h5>
+                        <p class="card-text">Futuro texto</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="text-decoration-none text-dark">Ver mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <div class="container d-flex justify-content-center mx-4">
+                <div class="card card text-dark bg-danger mb-3" style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title text">Valor total</h5>
+                        <p class="card-text">Futuro texto.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="text-decoration-none text-dark">Ver mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- template/nav.php
+++ template/nav.php
@@ -1,68 +1,73 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Sidebar</title>
+    <style>
+        .letra {
+            font-size: 20px;
+        }
+    </style>
 </head>
-<body>
-    <nav id="sidebar">
-        <div id="sidebar_content">
-            <div id="user">
-                <img src="../img/crf.png" id="user_avatar" alt="Avatar">
-    
-                <p id="user_infos">
-                    <span class="item-description perfil">
-                        Perfil
-                    </span>
-                    <span class="item-description">
-                        <?php echo $_SESSION['nome']; ?>
-                    </span>
-                </p>
-            </div>
-    
-            <ul id="side_items">
-                <li class="side-item active">
-                    <a href="index.php">
-                        <i class="fa-solid fa-house"></i>
-                        <span class="item-description">
-                            Inicio
-                        </span>
-                    </a>
-                </li>
-                <li class="side-item">
-                    <a href="#">
-                        <i class="fa-solid fa-tag"></i>
-                        <span class="item-description">
-                            Categorias
-                        </span>
-                    </a>
-                </li>
-                <li class="side-item">
-                    <a href="#">
-                        <i class="fa-solid fa-box"></i>
-                        <span class="item-description">
-                            Produtos
-                        </span>
-                    </a>
-                </li>
-  
-            </ul>
-    
-            <button id="open_btn">
-                <i id="open_btn_icon" class="fa-solid fa-chevron-right"></i>
-            </button>
-        </div>
 
-        <div id="logout">
-            <button id="logout_btn">
-                <i class="fa-solid fa-right-from-bracket"></i>
-                <a href="../verificar/logout.php" class="item-description">Sair</a>
-            </button>
+<body>
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <div class="d-flex justify-content-start">
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand mx-2" href="#">
+                    <img src="../img/crf.png" alt="Bootstrap" width="40" height="40">
+                </a>
+            </div>
+            <div class="d-flex justify-content-end">
+                <li class="nav-item dropdown d-flex align-items-center mx-2">
+                    <a class="nav-link dropdown-toggle text-bg-dark " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php echo $_SESSION['nome']; ?>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Home</a></li>
+                        <li><a class="dropdown-item" href="#">Produtos</a></li>
+                        <li><a class="dropdown-item" href="#">Categorias</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">sair</a></li>
+                    </ul>
+                </li>
+                <button type="button" class="btn btn-danger  mx-3"><a href="../verificar/logout.php" class="text-decoration-none text-white ">Sair</a></button>
+            </div>
+            <div class="offcanvas offcanvas-start text-bg-dark" style="width: 300px;" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active d-flex align-items-center letra" aria-current="page" href="index.php">
+                                <i class="fa-solid fa-house me-3"></i>Início</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active d-flex align-items-center letra" href="#">
+                                <i class="fa-solid fa-box me-3"></i>Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex active align-items-center letra" href="#">
+                                <i class="fa-solid fa-tag me-3"></i>Categorias</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
         </div>
     </nav>
-    <script src="../js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
